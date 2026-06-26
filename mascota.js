3451b3623bf812ae6e6328291cb5978cbff3f082
Sara Vega <209210282+saraeveal@users.noreply.github.com>
2026-06-25 22:20:50 -0400

    feat: etapa 4 - metodos jugar y dormir

--- mascota.js
+++ mascota.js
@@ -1,4 +1,3 @@
-//Etapa 3 · alimentar()
 const mascota = {
     nombre: "Pelusa",
     tipo: "gato",
@@ -18,39 +17,49 @@ const mascota = {
         `${this.nombre} comió 🍖 — Hambre: ${this.hambre}`
     );
     },
+
+    jugar() {
+    this.felicidad = Math.min(
+        100,
+        this.felicidad + 20
+    );
+
+    this.energia = Math.max(
+        0,
+        this.energia - 15
+    );
+
+    console.log(
+        `${this.nombre} jugó 🎾 — Felicidad: ${this.felicidad}, Energía: ${this.energia}`
+    );
+    },
+
+    dormir() {
+    this.energia = Math.min(
+        100,
+        this.energia + 30
+    );
+
+    console.log(
+        `${this.nombre} durmió 😴 — Energía: ${this.energia}`
+    );
+    },
 };
 
 //Etapa 1 · Nace tu mascota
-const mascota1 = {
-    nombre: "Pelusa",
-    tipo: "gato",
 
-    hambre: 50,
-    felicidad: 50,
-    energia: 50
-};
 //Notación punto
 console.log("Nombre:", mascota.nombre);
 //Notación corchete
 console.log("Tipo:", mascota["tipo"]);
 
 //Etapa 2 · Método describir() y this
-const mascota2 = {
-    nombre: "Pelusa",
-    tipo: "gato",
-
-    hambre: 50,
-    felicidad: 50,
-    energia: 50,
-
-    describir() {
-        return `${this.nombre} es un ${this.tipo}.`;
-    }
-};
-
 console.log(mascota.describir());
 
 //Etapa 3 · alimentar()
+mascota.alimentar();
+mascota.alimentar();
 
-mascota.alimentar();
-mascota.alimentar();
+//Etapa 4 · jugar() y dormir()
+mascota.jugar();
+mascota.dormir();
