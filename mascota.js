bb443a95bb96ad4fa7e98f9a19965c86e1c0a3c9
Sara Vega <209210282+saraeveal@users.noreply.github.com>
2026-06-25 22:08:11 -0400

    feat: etapa 1 - objeto mascota

---
+++ mascota.js
@@ -0,0 +1,13 @@
+//Etapa 1 · Nace tu mascota
+const mascota = {
+    nombre: "Pelusa",
+    tipo: "gato",
+
+    hambre: 50,
+    felicidad: 50,
+    energia: 50
+};
+//Notación punto
+console.log("Nombre:", mascota.nombre);
+//Notación corchete
+console.log("Tipo:", mascota["tipo"]);
