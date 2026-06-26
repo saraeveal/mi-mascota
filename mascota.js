34398ecf1fd3a19c81f12fa9310dd7a82a4f16f3
Sara Vega <209210282+saraeveal@users.noreply.github.com>
2026-06-25 22:11:26 -0400

    feat: etapa 2 - metodo describir con this

--- mascota.js
+++ mascota.js
@@ -1,5 +1,5 @@
 //Etapa 1 · Nace tu mascota
-const mascota = {
+const mascota1 = {
     nombre: "Pelusa",
     tipo: "gato",
 
@@ -11,3 +11,19 @@ const mascota = {
 console.log("Nombre:", mascota.nombre);
 //Notación corchete
 console.log("Tipo:", mascota["tipo"]);
+
+//Etapa 2 · Método describir() y this
+const mascota = {
+    nombre: "Pelusa",
+    tipo: "gato",
+
+    hambre: 50,
+    felicidad: 50,
+    energia: 50,
+
+    describir() {
+        return `${this.nombre} es un ${this.tipo}.`;
+    }
+};
+
+console.log(mascota.describir());
