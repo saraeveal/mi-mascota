d737e74bda84c3e2de66ea1e7d1b6abddd422a69
Sara Vega <209210282+saraeveal@users.noreply.github.com>
2026-06-25 22:19:08 -0400

    feat: etapa 3 - metodo alimentar

--- mascota.js
+++ mascota.js
@@ -1,3 +1,25 @@
+//Etapa 3 · alimentar()
+const mascota = {
+    nombre: "Pelusa",
+    tipo: "gato",
+
+    hambre: 50,
+    felicidad: 50,
+    energia: 50,
+
+    describir() {
+        return `${this.nombre} es un ${this.tipo}.`;
+    },
+
+    alimentar() {
+    this.hambre = Math.max(0, this.hambre - 20);
+
+    console.log(
+        `${this.nombre} comió 🍖 — Hambre: ${this.hambre}`
+    );
+    },
+};
+
 //Etapa 1 · Nace tu mascota
 const mascota1 = {
     nombre: "Pelusa",
@@ -13,7 +35,7 @@ console.log("Nombre:", mascota.nombre);
 console.log("Tipo:", mascota["tipo"]);
 
 //Etapa 2 · Método describir() y this
-const mascota = {
+const mascota2 = {
     nombre: "Pelusa",
     tipo: "gato",
 
@@ -27,3 +49,8 @@ const mascota = {
 };
 
 console.log(mascota.describir());
+
+//Etapa 3 · alimentar()
+
+mascota.alimentar();
+mascota.alimentar();
